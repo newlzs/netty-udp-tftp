65abcf6c159bb2164a53061497caca8e9aa39765
newlzs <1581004928@qq.com>
2021-06-29 00:49:14 +0800

    fix 客户端阻塞与服务端文件不存在异常处理

---
+++ common/src/main/java/org/lizishi/netty/udp/tftp/common/utils/ErrorUtils.java
@@ -0,0 +1,9 @@
+package org.lizishi.netty.udp.tftp.common.utils;
+
+/**
+ * @author Lzs
+ * @date 2021/6/28 21:35
+ * @description
+ */
+public class ErrorUtils {
+}
---
+++ model/src/main/java/org/lizishi/netty/udp/tftp/enums/Error.java
@@ -0,0 +1,9 @@
+package org.lizishi.netty.udp.tftp.enums;
+
+/**
+ * @author Lzs
+ * @date 2021/6/28 21:26
+ * @description
+ */
+public class ErrorCode {
+}
